120fe6d4906dd96dfba15fac96ee012f3d9b4a48
Nasvad <davifreireangelim@gmail.com>
2022-05-23 15:21:54 -0300

    database

---
+++ bd.php
@@ -0,0 +1,16 @@
+<?php
+
+//Pagina de conexao com o DB
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     ="blog";
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Conectado com Sucesso";
+} catch(PDOException $e) {
+  echo "Falha na conexão: " . $e->getMessage();
+}
+?>
