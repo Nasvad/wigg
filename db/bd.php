7c123a4594fb6030ffd923824289ca194b9ef4fd
Alexsandro Saymon <unknowaved@gmail.com>
2022-05-28 15:45:22 -0300

    fixado bd.php

--- db/bd.php
+++ db/bd.php
@@ -6,10 +6,7 @@
 $servername = "localhost";
 $username   = "root";
 $password   = "";
-<<<<<<< HEAD:db/bd.php
 $dbname     ="wigg_1";
-=======
->>>>>>> 9938e42663c5428cadc9842803160775895734c4:bd.php
 try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname;charset=utf8", $username, $password);
   // set the PDO error mode to exception
