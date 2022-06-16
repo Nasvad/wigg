28e0b94082ee83279c4e404d02d61604be85d8be
Alexsandro Saymon <unknowaved@gmail.com>
2022-06-16 19:15:15 -0300

    página 404 adicionada

---
+++ 404.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <style>
+            body {
+                margin: 0;
+                background-color: black;
+                display: flex;
+                justify-content: center;
+                
+            }
+            img {
+                background-color: black;
+                height: 97vh;
+            }
+        </style>
+    </head>
+    <body>
+        <img src="img/404.png" />
+    </body>
+</html>
