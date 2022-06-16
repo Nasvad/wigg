1f086e877945af297cfaee5cdb6e9ce0f3f35e4f
Nasvad <davifreireangelim@gmail.com>
2022-06-16 15:52:06 -0300

    navbar cleanup

--- contact.php
+++ contact.php
@@ -64,7 +64,6 @@
                 <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
                     <div class="navbar-nav ml-auto">
                         <a href="index.php" class="nav-item nav-link active">Início</a>
-                        <a href="#" class="nav-item nav-link">Serviços</a>
                         <a href="login.php" class="nav-item nav-link">Faça seu login</a>
                                            
                     </div>
