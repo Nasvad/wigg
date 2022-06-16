9936c124b7b82eacf09c75e18a93b34d8e6b951c
Nasvad <davifreireangelim@gmail.com>
2022-06-16 15:41:27 -0300

    about finalizado

--- about.php
+++ about.php
@@ -84,11 +84,10 @@
             <div class="container">
                 <div class="row">
                     <div class="col-12">
-                        <h2>About Us</h2>
+                        <h2>Sobre a Wigg</h2>
                     </div>
                     <div class="col-12">
-                        <a href="">Home</a>
-                        <a href="">About Us</a>
+                        <a href="">Início</a>
                     </div>
                 </div>
             </div>
@@ -102,22 +101,20 @@
                 <div class="row align-items-center">
                     <div class="col-lg-5 col-md-6">
                         <div class="about-img">
-                            <img src="img/about.jpg" alt="Image">
+                            <img src="img/altwigg.png" alt="Image">
                         </div>
                     </div>
                     <div class="col-lg-7 col-md-6">
                         <div class="section-header text-left">
-                            <p>Learn About Us</p>
-                            <h2>25 Years Experience</h2>
+                            <p>Um produto Space.IO</p>
+                            <h2>A Wigg veio para facilitar sua vida!</h2>
                         </div>
                         <div class="about-text">
                             <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit non vulputate. Aliquam metus tortor, auctor id gravida condimentum, viverra quis sem.
+                            O Wigg veio para sanar uma necessidade nichada com capacidade para se expandir, buscando agilizar, diversificar, facilitar/proporcionar visibilidade mais facilmente entre o meio e seus interessados, facilitando para o cliente usuário final, que, consegue encontrar colaboradores com diferentes preços e especializações e facilitando para o colaborador, que, tem na plataforma, uma visibilidade de clientes voltadas para a área, abrindo margem para crescimento profissional e expansão de negócios no meio virtual e consequentemente físico. 
                             </p>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec pretium mi. Curabitur facilisis ornare velit non vulputate. Aliquam metus tortor, auctor id gravida condimentum, viverra quis sem. Curabitur non nisl nec nisi scelerisque maximus. Aenean consectetur convallis porttitor. Aliquam interdum at lacus non blandit.
-                            </p>
-                            <a class="btn" href="">Learn More</a>
+                            
+                            
                         </div>
                     </div>
                 </div>
@@ -126,7 +123,7 @@
         <!-- About End -->
 
 
-        <!-- Team Start -->
+        <!-- Team Start 
         <div class="team">
             <div class="container">
                 <div class="section-header text-center">
@@ -181,7 +178,7 @@
                 </div>
             </div>
         </div>
-        <!-- Team End -->
+         -->
 
 
         <!-- Footer Start -->
