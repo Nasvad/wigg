6fc54c7b6c33fa8a4068a876632949e932f0bb97
Alexsandro Saymon <unknowaved@gmail.com>
2022-06-17 00:25:29 -0300

    adicionado fale_conosco.php

---
+++ fale_conosco.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<?php session_start(); ?>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Wigg - Trabalhe com a gente.</title>
+        <meta content="width=device-width, initial-scale=1.0" name="viewport">
+        <meta content="Free Website Template" name="keywords">
+        <meta content="Free Website Template" name="description">
+
+        <!-- Favicon -->
+        <link href="img/favicon.ico" rel="icon">
+
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
+
+        <!-- CSS Libraries -->
+        <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" rel="stylesheet">
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+        <link href="lib/animate/animate.min.css" rel="stylesheet">
+        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+        <link href="css/custom.css" rel="stylesheet">
+
+        <!-- Template Stylesheet -->
+        <link href="css/style.css" rel="stylesheet">
+        <script>
+            function alerta_email() {
+                alert("Email enviado!");
+            }
+        </script>
+    </head>
+
+    <body>
+        <div class="top-bar d-none d-md-block">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-6">
+                        
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="top-bar-right">
+                            <div class="social">
+                                 <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                                    <a href="https://facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="https://instagram.com/"><i class="fab fa-instagram"></i></a>
+                                    <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+                                    
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Top Bar End -->
+
+        <!-- Nav Bar Start -->
+        <div class="navbar navbar-expand-lg bg-dark navbar-dark">
+            <div class="container-fluid">
+                <a href="index.html" class="navbar-brand">WIGG</a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto">
+                        <a href="index.html" class="nav-item nav-link active">Início</a>
+                        <a href="#" class="nav-item nav-link">Serviços</a>
+                        <a href="login.html" class="nav-item nav-link">Faça seu login</a>
+                                           
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Nav Bar End -->
+
+
+
+        <!-- Page Header Start -->
+        <div class="page-header">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <h2>Entre em contato conosco</h2>
+                    </div>
+                    <div class="col-12">
+                        <a href="index.html">Início</a>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Page Header End -->
+
+
+        <!-- Contact Start -->
+        <div class="section-header text-center" style="margin-top: 90px;">
+            <p>Seja um colaborador Wigg</p>
+            <h2>Preencha com seus dados e trabalhe com a gente!</h2>
+        </div>
+        <div class="contact" style="margin-bottom: 90px;">
+            <div class="container-fluid">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-md-4"></div>
+                        <div class="col-md-8">
+                            <div class="contact-form">
+                                <div id="success"><?php 
+                                    if(isset($_SESSION["email_enviado"]) && $_SESSION["email_enviado"]) {
+                                        echo '<p id="cadastro_sucesso">Email enviado com sucesso!</p>';
+                                    }
+
+                                    unset($_SESSION["email_enviado"])
+                                ?></div>
+                                <form name="sentMessage" id="contactForm" method="POST" action="mail/contact.php">
+                                    <div class="control-group">
+                                        <input type="text" name="nome" class="form-control" id="name" placeholder="Seu nome completo" required="required" />
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="control-group">
+                                        <input type="email" name="email" class="form-control" id="email" placeholder="Seu Email" required="required"/>
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div class="control-group">
+                                        <textarea type="text" name="duvida" class="form-control" id="corpname" placeholder="Sua dúvida" required="required"></textarea>
+                                        <p class="help-block text-danger"></p>
+                                    </div>
+                                    <div>
+                                        <button class="btn" type="submit" id="sendMessageButton">Vamos começar!</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Contact End -->
+
+
+        <!-- Footer Start -->
+        <div class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-7">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="footer-contact">
+                                    <h2>Sobre nós</h2>
+                                    <p><i class="fa fa-map-marker-alt"></i>Juazeiro do Norte, Ceará, Brasil</p>
+                                    <p><i class="fa fa-phone-alt"></i>+55(87)99233-4234</p>
+                                    <p><i class="fa fa-envelope"></i>wigg@spaceio.com</p>
+                                    <div class="footer-social">
+                                       <a href="https://twitter.com/"><i class="fab fa-twitter"></i></a>
+                                    <a href="https://facebook.com/"><i class="fab fa-facebook-f"></i></a>
+                                    <a href="https://instagram.com/"><i class="fab fa-instagram"></i></a>
+                                    <a href="https://www.youtube.com/"><i class="fab fa-youtube"></i></a>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="footer-link">
+                                    <h2>Acesso rápido</h2>
+                                    <a href="">Termos de uso</a>
+                                    <a href="">Política de Privacidade</a>
+                                    <a href="">Cookies</a>
+                                    <a href="">Ajuda</a>
+                                    <a href="">FAQ</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-5">
+                        <div class="footer-newsletter">
+                            <h2>Newsletter</h2>
+                            <p>
+                                Assine nossa newsletter e fique por dentro de todas as novidades da WIGG
+                            </p>
+                            <div class="form">
+                                <input class="form-control" placeholder="Insira seu email">
+                                <button class="btn">Enviar</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="container copyright">
+                <div class="row">
+                    <div class="col-md-6">
+                        <p>&copy; <a href="index.html">Wigg by Space.IO</a>, Todos os direitos reservados.</p>
+                    </div>
+                    <div class="col-md-6">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+        <a href="#" class="back-to-top"><i class="fa fa-chevron-up"></i></a>
+
+        <!-- JavaScript Libraries -->
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+        <script src="lib/easing/easing.min.js"></script>
+        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+        <script src="lib/isotope/isotope.pkgd.min.js"></script>
+        <script src="lib/lightbox/js/lightbox.min.js"></script>
+
+        <!-- Contact Javascript File -->
+        <script src="mail/jqBootstrapValidation.min.js"></script>
+        <!-- <script src="mail/contact.js"></script> -->
+
+        <!-- Template Javascript -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
--- mail/contact.php
+++ mail/contact.php
@@ -1,20 +1,26 @@
 <?php
-if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+session_start();
+
+/*if(empty($_POST['name']) || empty($_POST['subject']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
-}
+}*/
 
-$name = strip_tags(htmlspecialchars($_POST['name']));
+$name = strip_tags(htmlspecialchars($_POST['nome']));
 $email = strip_tags(htmlspecialchars($_POST['email']));
-$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
+//$m_subject = strip_tags(htmlspecialchars($_POST['subject']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 $to = "info@example.com"; // Change this email to your //
-$subject = "$m_subject:  $name";
-$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
-$header = "From: $email";
-$header .= "Reply-To: $email";	
+$subject = "Assunto:  Dúvida de usuário";
+$body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nMessage: $message";
+/*$header = "From: $email";
+$header .= "Reply-To: $email";*/
 
-if(!mail($to, $subject, $body, $header))
-  http_response_code(500);
+if(!mail($to, $subject, $body)) http_response_code(500);
+$_SESSION["email_enviado"] = true;
+
+header("Location: ../fale_conosco.php");
 ?>
+
+
